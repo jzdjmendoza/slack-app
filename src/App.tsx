816c535add5be703341a07313d0a21b81ceb345e
May Four Apple Espinosa <mayfourapple@gmail.com>
2022-07-06 22:52:54 +0800

    Add mainpage component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LogInRegister from './components/LogInRegister'
+import MainPage from './components/MainPage'
 
 function App() {
   return (
     <div className="App">
       <LogInRegister />
+      <MainPage/>
     </div>
   );
 }
