4638d990e534ad6be3e8485d5f4f580f880fba99
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-07 13:48:56 +0800

    Fix Login and Register

--- src/components/LogInRegister.tsx
+++ src/components/LogInRegister.tsx
@@ -2,71 +2,85 @@ import React from "react";
 import { useState } from "react";
 
 const LogInRegister = () => {
-    const [login, setLogin] = useState(true);
-    const onClick = () => {
-        
+    const [isLogin, setIsLogin] = useState(true);
+    const toggleForm = () => {
+        setIsLogin(!isLogin);
     }
 
     return (
         <>
-            <div className="container">
-            <div className="forms-container">
-                <div className="login-register">
+            <div className={"container " + (isLogin ? '' : 'register-mode')}>
+                <div className="forms-container">
+                    <div className="login-register">
                     <form id="loginForm" className="log-in-form">
                         <h2 className="title">Log in</h2>
                         <div className="input-field">
-                            <input type="email" name="emailLogin" id="emailLogin" placeholder="Email" />
+                        <i className="fas fa-envelope"></i>
+                        <input type="email" name="emailLogin" id="emailLogin" placeholder="Email"/>
                         </div>
                         <div className="input-field">
-                            <input type="password" name="passwordLogin" id="passwordLogin" placeholder="Password" />
+                        <i className="fas fa-lock"></i>
+                        <input type="password" name="passwordLogin" id="passwordLogin" placeholder="Password"/>
                         </div>
-                        <button type="submit" value="Login" className="btn solid">
-                            <p>Log In</p>
-                        </button>
+                        <input type="submit" value="Login" className="btn solid"/>
                     </form>
                     <form id="registerForm" className="register-form">
                         <h2 className="title">Register</h2>
                         <div className="input-field">
-                            <input type="text" name="FullName" id="FullName" placeholder="Full Name" />
+                        <i className="fas fa-user"></i>
+                        <input type="text" name="firstName" id="firstName" placeholder="First Name"/>
+                        </div><div className="input-field">
+                        <i className="fas fa-user"></i>
+                        <input type="text" name="lastName" id="lastName" placeholder="Last Name"/>
                         </div>
                         <div className="input-field">
-                            <input type="email" name="email" id="email" placeholder="Email" />
+                        <i className="fas fa-envelope"></i>
+                        <input type="email" name="email" id="email" placeholder="Email"/>
                         </div>
                         <div className="input-field">
-                            <input type="password" name="password" id="password" placeholder="Password" />
+                        <i className="fas fa-lock"></i>
+                        <input type="password" name="password" id="password" placeholder="Password"/>
                         </div>
                         <div className="input-field">
-                            <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" />
+                        <i className="fas fa-mobile"></i>
+                        <input type="text" name="mobileNo" id="mobileNo" placeholder="Mobile Number"/>
                         </div>
-                        <button type="submit" className="btn solid" value="Register">
-                            <p> Register</p>
-                        </button>
+                        <div className="input-field">
+                        <i className="fas fa-book"></i>
+                        <select required className="form-control" name="sel1" id="sel1">
+                        </select>
+                        </div>
+                        <input type="submit" className="btn solid" value="Register"/>
                     </form>
+                    </div>
                 </div>
-            </div>
 
-            <div className="panels-container">
-                <div className="panel left-panel">
-                <div className="content">
-                    <h3>New here?</h3>
-                    <p>Create an account and experience a whole new world! We just need a few details about you.</p>
-                    <button className="btn transparent" id="register-btn">
-                        Register Here!
-                    </button>
+                <div className="panels-container">
+                    <div className="panel left-panel">
+                    <div className="content">
+                        <h3>New here ?</h3>
+                        <p>
+                        Create an account and experience a whole new world! We just need a few details about you and we swear we won't use them to stalk you.
+                        </p>
+                        <button className={"btn transparent "+ (isLogin ? '' : 'register-mode') } id="register-btn" onClick={toggleForm}>
+                        Register
+                        </button>
+                    </div>
+                    <img src="img/log.svg" className="image" alt=""/>
+                    </div>
+                    <div className="panel right-panel">
+                    <div className="content">
+                        <h3>Already have an account ?</h3>
+                        <p>
+                        Well, well, well. Look who's back. Head here to access your account.
+                        </p>
+                        <button className="btn transparent" id="log-in-btn" onClick={toggleForm}>
+                        Log In
+                        </button>
+                    </div>
+                    <img src="img/register.svg" className="image" alt=""/>
+                    </div>
                 </div>
-                <img src="img/log.svg" className="image" alt="" />
-                </div>
-                <div className="panel right-panel">
-                <div className="content">
-                    <h3>Already have an account ?</h3>
-                    <p>Welcome back. Head here to access your account.</p>
-                    <button className="btn transparent" id="log-in-btn" onClick={onClick}>
-                        Log In Here!
-                    </button>
-                </div>
-                <img src="img/register.svg" className="image" alt="" ></img>
-                </div>
-            </div>
             </div>
         </>
     )
