43252225978419e9f009012a3afa20db3be5dac3
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-08 18:06:41 +0800

    Add Navbar

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,13 +2,12 @@ import { Session } from 'inspector'
 import React, { useContext } from 'react'
 import SessionContext from '../contexts/SessionContext'
 import Header from './Header'
-import Sidebar from './Sidebar'
+
 
 export default function MainPage() {
   return(
     <>
       <Header />
-      <Sidebar />
     </>
   )
 }
