5c3002797fde202af51d5c58a838d4405922d750
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-08 15:01:28 +0800

    Add Sidebar file

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -25,7 +25,16 @@ export default function MainPage() {
     })
 
   return(
-    <div className="MainPage">
+    <div className="MainPage bg-sky-100 w-100 h-100">
+      <span className="w-full">Header</span>
+      <div className='flex flex-wrap'>
+        <div className="bg-slate-300">
+          <span>Sidebar</span>
+        </div>
+        <div className="bg-slate-300">
+          <span>Main Chat</span>
+        </div>
+      </div>
     </div>
   )
 }
