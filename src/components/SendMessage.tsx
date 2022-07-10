861a6c0d0ce6697ab1cd01d9c90e29cd78b9d75c
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-11 01:56:32 +0800

    Change input text to text area

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -5,7 +5,7 @@ export default function SendMessage() {
         <>
             <div className="w-full items-center text-center px-4 mb-4 sticky bottom-0 bg-white">
                 <form className="flex text-gray-900 text-xl border-4 h-fit rounded-lg border-slate-300">
-                    <input type="text" className="w-full h-fit focus:outline-none p-1" />
+                    <textarea className="w-full h-fit focus:outline-none p-1" />
                     <button type="button" className="inline-flex items-center justify-center rounded-lg px-2 py-1 transition duration-500 ease-in-out text-white bg-sky-500 hover:bg-blue-400 focus:outline-none">
                         <span className="font-bold text-base">Send</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90 md:h-4">
