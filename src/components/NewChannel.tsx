51ac216ea3b88450a92809cbf3fd14dbc20a0f74
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-12 15:37:58 +0800

    Remove sample data options for drop down

--- src/components/NewChannel.tsx
+++ src/components/NewChannel.tsx
@@ -63,21 +63,7 @@ export default function NewChannel() {
                      }))
         setUsers(data)
         return users
-        // console.log(users)
     }
-
-    const colourOptions = [
-        { value: "ocean1", label: "Ocean" },
-        { value: "blue", label: "Blue" },
-        { value: "purple", label: "Purple" },
-        { value: "red", label: "Red" },
-        { value: "orange", label: "Orange" },
-        { value: "yellow", label: "Yellow" },
-        { value: "green", label: "Green" },
-        { value: "forest", label: "Forest" },
-        { value: "slate", label: "Slate" },
-        { value: "silver", label: "Silver" }
-    ];
 
     useEffect(() => {
         fetchUsers()
