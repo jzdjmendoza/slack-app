a80c27d98a9783e461191b258cc652fea739ae65
Jaz Mendoza <jazynnemendoza@gmail.com>
2022-07-13 18:20:48 +0800

    Add enter function to send message

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -31,11 +31,17 @@ export default function SendMessage() {
         setMessage('')
     }
 
+    const enterMessage = (event: any) => {
+        if (event.key === 'Enter') {
+            sendMessage(event)
+        }
+    }
+
     return (
         <>
             <div className="w-full items-center text-center px-4 mb-4 sticky bottom-0 bg-white">
                 <form className="flex text-gray-900 text-xl border-4 h-fit rounded-lg border-slate-300">
-                    <textarea value={message} className="w-full h-fit focus:outline-none p-1" placeholder="Type your message..." onChange={event => setMessage(event.target.value)}/>
+                    <textarea value={message} className="w-full h-fit focus:outline-none p-1" placeholder="Type your message..." onKeyPress={enterMessage} onChange={event => setMessage(event.target.value)}/>
                     <button type="button" className="inline-flex items-center justify-center rounded-lg px-2 py-1 transition duration-500 ease-in-out text-white bg-slack-500 hover:bg-slack-300 focus:outline-none" onClick={sendMessage}>
                         <span className="font-bold text-base">Send</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90 md:h-4">
